82cf6adec3a8f6bb7f3debaca4770411f78cd7c6
KatHewitt <44177980+KatHewitt@users.noreply.github.com>
2018-12-04 16:07:13 +0000

    final updated

--- Model_5.py
+++ Model_5.py
@@ -17,14 +17,7 @@ import matplotlib.backends.backend_tkagg
 import requests
 import bs4
 
-#scrape a web page to get the agent starting locations 
-r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
-content = r.text
-soup = bs4.BeautifulSoup(content, 'html.parser')
-td_ys = soup.find_all(attrs={"class" : "y"})
-td_xs = soup.find_all(attrs={"class" : "x"})
-print(td_ys)
-print(td_xs)
+
 
 
 environment = []
@@ -39,82 +32,98 @@ with open('in.txt') as f:
         environment.append(rowlist)
 
 matplotlib.use('TkAgg')
-matplotlib.pyplot.imshow(environment)
-matplotlib.pyplot.show()
+#matplotlib.pyplot.imshow(environment)
+#matplotlib.pyplot.show()
 
 
-def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a.x - agents_row_b.x)**2) + 
-    ((agents_row_a.y - agents_row_b.y)**2))**0.5
+#def distance_between(agents_row_a, agents_row_b):
+#    return (((agents_row_a.x - agents_row_b.x)**2) + 
+#    ((agents_row_a.y - agents_row_b.y)**2))**0.5
 
 num_of_agents = 10
 num_of_iterations = 100
 neighbourhood = 20
 agents = []
 
+
+
 # Make the agents.
+
+#scrape a web page to get the agent starting locations 
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+#print(td_ys)
+#print(td_xs)
+
 for i in range(num_of_agents):
     #instead of random starting positions, use locations from scraped web page
     y = int(td_ys[i].text)
     x = int(td_xs[i].text)
-    agents.append(agentframework3.agent(environment,agents))
+    agents.append(agentframework3.agent(environment,agents, y, x))
     
-for a in agents:
-    print(a.getx(), a.gety())
 
 
-print ("----------",agents[0].getx())
-print ("----------",agents[0].gety())
+#for a in agents:
+#    print(a.getx(), a.gety())
+#print ("----------",agents[0].getx())
+#print ("----------",agents[0].gety())
+
+
 
 # Move the agents.
-for j in range(num_of_iterations):
-    random.shuffle(agents)
-    for i in range(num_of_agents):
-        #randomise the order in which agents are processed each iteration
+#for j in range(num_of_iterations):
+#    random.shuffle(agents)
+#    for i in range(num_of_agents):
+#        #randomise the order in which agents are processed each iteration
+#
+#        #agents[i].move()
+#        #substituted for this one - advanced model 
+#        #print to find out if it does
+#        agents[i].move_faster()
+#        agents[i].eat()
+#        agents[i].share_with_neighbourhood(neighbourhood)
 
-        #agents[i].move()
-        #substituted for this one - advanced model 
-        #print to find out if it does
-        agents[i].move_faster()
-        agents[i].eat()
-        agents[i].share_with_neighbourhood(neighbourhood)
+#matplotlib.pyplot.xlim(0, 99)
+#matplotlib.pyplot.ylim(0, 99)
+#matplotlib.pyplot.imshow(environment)
 
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.ylim(0, 99)
-matplotlib.pyplot.imshow(environment)
-for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
-matplotlib.pyplot.show()
-
-for agents_row_a in agents:
-    for agents_row_b in agents:
-        distance = distance_between(agents_row_a, agents_row_b)
-        
-a = agentframework3.agent(environment,agents)
-print (a.y, a.x)
-a.move()
-print (a.y, a.x)
+#for i in range(num_of_agents):
+#    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
+#matplotlib.pyplot.show()
+#
+#for agents_row_a in agents:
+#    for agents_row_b in agents:
+#        distance = distance_between(agents_row_a, agents_row_b)
+#        
+#a = agentframework3.agent(environment,agents)
+#print (a.y, a.x)
+#a.move()
+#print (a.y, a.x)
 
 
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_axes([0,0,1,1])
-agents = []
-for i in range(num_of_agents):
-    agents.append(agentframework3.agent(environment,agents))
+
+#agents = []
+#for i in range(num_of_agents):
+#    agents.append(agentframework3.agent(environment,agents))
 
 carry_on = True
 
 def update (frame_number):
+    global carry_on
     fig.clear() 
-    matplotlib.pyplot.xlim(0, 99)
-    matplotlib.pyplot.ylim(0, 99)
+    matplotlib.pyplot.xlim(0, 299)
+    matplotlib.pyplot.ylim(0, 299)
     matplotlib.pyplot.imshow(environment)
 
     for i in range(num_of_agents):
         agents[i].move_faster()
         agents[i].eat()
-        agents[i].share_with_neighbourhood
-        matplotlib.pyplot.scatter(agents[i].getx(),agents[i].gety(),facecolors='none', edgecolors='black')
+        agents[i].share_with_neighbourhood(neighbourhood)
 #            if random.random() < 0.5:
 #                agents[i][0]  = (agents[i][0] + 1) % 99 
 #            else:
@@ -125,7 +134,7 @@ def update (frame_number):
 #            else:
 #                agents[i][1]  = (agents[i][1] - 1) % 99
         
-    if random.random()< 0.1:
+    if random.random()< 0.01:
         carry_on = False
         print("stopping condition")
         
@@ -133,10 +142,11 @@ def update (frame_number):
         matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
         print(agents[i].x,agents[i].y)
 
+
 def gen_function(b = [0]):
     a = 0
     global carry_on #not needed as were not assigning, but clearer
-    while (a<10) & (carry_on) :
+    while (a<1000) & (carry_on) :
         yield a            #returns control and waits next call
         a = a+1
 
@@ -148,6 +158,7 @@ def gen_function(b = [0]):
 #produces animation 
 
 def run():
+    global animation
     #called by the GUI menu item run
     animation = matplotlib.animation.FuncAnimation (fig, update, frames=gen_function, repeat=False)
     canvas.show()
